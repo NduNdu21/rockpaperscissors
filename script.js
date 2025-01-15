8fc985137caf9e25fdd5e155e26f1a06ac256c33
NduNdu21 <ndumisobm@Ndus-Lenovo.localdomain>
2025-01-15 11:39:51 +0000

    Added logic to get the computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //JavaScript code for rock paper scissors game
+console.log("Hello World!");
+
+function getComputerChoice() {
+    let play = Math.floor(Math.random() * 3);
+    if (play == 0) {
+        return "rock";
+    } else if (play == 1) {
+        return "paper";
+    } else if (play == 2) {
+        return "scissors";
+    }
+}
