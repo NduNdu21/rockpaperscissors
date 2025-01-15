ea88f69424f5823d89f9df3e06944cfa0322747a
NduNdu21 <ndumisobm@icloud.com>
2025-01-15 12:49:56 +0000

    Added logic to play multiple rounds

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function getComputerChoice() {
 console.log(getComputerChoice())
 
 function getHumanChoice() {
-    let choice = prompt("Let's play rock, paper, scissors!\nChoose your option by typing \"rock\", \"paper\" or \"scissors\" in lowercase").toLowerCase()
+    let choice = prompt("Let's play rock, paper, scissors!\nChoose your option by typing \"rock\", \"paper\" or \"scissors\" in lowercase\nType \"quit\" if you're done playing").toLowerCase()
 
     return choice
 }
@@ -23,3 +23,42 @@ console.log(getHumanChoice())
 let humanScore = 0
 let computerScore = 0
 
+function playRound(humanChoice, computerChoice) {
+    while (humanChoice !== "quit") {
+        switch (humanChoice) {
+            case "rock":
+                if (computerChoice === "rock") {
+                    return "Draw, try again!"
+                } else if (computerChoice === "paper") {
+                    computerChoice++
+                    return "You lose! Paper beats Rock"
+                } else if (computerChoice === "scissors") {
+                    humanScore++
+                    return "You win! Rock beats Scissors"
+                }
+            break;
+            case "paper":
+                if (computerChoice === "rock") {
+                    humanScore++
+                    return "You win! Paper beats Rock"
+                } else if (computerChoice === "paper") {
+                    return "Draw, try again!"
+                } else if (computerChoice === "scissors") {
+                    computerScore++
+                    return "You lose! Scissors beats Paper"
+                }
+            break;
+            case "scissors":
+                if (computerChoice === "rock") {
+                    computerScore++
+                    return "You lose! Rock beats Scissors"
+                } else if (computerChoice === "paper") {
+                    humanScore++
+                    return "You win! Scissors beats Paper"
+                } else if (computerChoice === "scissors") {
+                    return "Draw, try again!"
+                }
+            break;
+        }
+    }
+}
